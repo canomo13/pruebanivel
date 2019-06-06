30f552e03b69f71aced190addf526b602d2b2305
José Antonio Cano Molina <canomolinajoseantonio@gmail.com>
2019-06-06 07:21:55 +0200

    Add files via upload

--- src/componentes/carrusel.js
+++ src/componentes/carrusel.js
@@ -4,35 +4,35 @@ import { Carousel } from "react-responsive-carousel";
 export default () => (
   <Carousel autoPlay>
     <div>
-      <img src="https://bensbargains.net/thecheckout/wp-content/uploads/2013/07/luke.jpg" />
+      <img src="https://bensbargains.net/thecheckout/wp-content/uploads/2013/07/luke.jpg" style={width:150, height:'auto'} />
       <p className="legend">Luke Skywalker</p>
     </div>
     <div>
-      <img src="https://bensbargains.net/thecheckout/wp-content/uploads/2013/07/han-solo.jpg" />
+      <img src="https://bensbargains.net/thecheckout/wp-content/uploads/2013/07/han-solo.jpg" style={width:150, height:'auto'} />
       <p className="legend">Han Solo</p>
     </div>
     <div>
-      <img src="http://bensbargains.net/thecheckout/wp-content/uploads/2013/07/chewbacca.jpg" />
+      <img src="http://bensbargains.net/thecheckout/wp-content/uploads/2013/07/chewbacca.jpg" style={width:150, height:'auto'} />
       <p className="legend">Chewbacca</p>
     </div>
     <div>
-      <img src="https://bensbargains.net/thecheckout/wp-content/uploads/2013/07/c3po.jpg" />
+      <img src="https://bensbargains.net/thecheckout/wp-content/uploads/2013/07/c3po.jpg" style={width:150, height:'auto'} />
       <p className="legend">C3PO</p>
     </div>
     <div>
-      <img src="https://bensbargains.net/thecheckout/wp-content/uploads/2013/07/leia.jpg" />
+      <img src="https://bensbargains.net/thecheckout/wp-content/uploads/2013/07/leia.jpg" style={width:150, height:'auto'} />
       <p className="legend">Leia Organa</p>
     </div>
     <div>
-      <img src="https://bensbargains.net/thecheckout/wp-content/uploads/2013/07/r2t2.jpg" />
+      <img src="https://bensbargains.net/thecheckout/wp-content/uploads/2013/07/r2t2.jpg" style={width:150, height:'auto'}/>
       <p className="legend">R2-d2</p>
     </div>
     <div>
-      <img src="https://bensbargains.net/thecheckout/wp-content/uploads/2013/07/yoda.jpg" />
+      <img src="https://bensbargains.net/thecheckout/wp-content/uploads/2013/07/yoda.jpg" style={width:150, height:'auto'}/>
       <p className="legend">Yoda</p>
     </div>
     <div>
-      <img src="https://bensbargains.net/thecheckout/wp-content/uploads/2013/07/darth-vader.jpg" />
+      <img src="https://bensbargains.net/thecheckout/wp-content/uploads/2013/07/darth-vader.jpg" style={width:150, height:'auto'} />
       <p className="legend">Darth Vader</p>
     </div>
 
